f4c479a30448a9d1f58533ee79f8cdca1ce9a0f9
John Koster <john@stillat.com>
2024-06-05 11:19:31 -0500

    New example

---
+++ resources/views/blade_template_blade_layout.blade.php
@@ -0,0 +1,11 @@
+@extends('blade_layout')
+
+@push('the_stack')
+	<br /> * Pushing content from a Blade template.
+@endpush
+
+@include('antlers_partial')
+
+@prepend('the_stack')
+	<br /> * Prepending content from a Blade template.
+@endprepend
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 // Route::statamic('example', 'example-view', [
 //    'title' => 'Example'
 // ]);
+
+
+Route::get('blade-route', function () {
+    return view('blade_template_blade_layout');
+});
